4dfbf9fce903776882a64747c00261ee51eb0135
Rick <dirklo@gmail.com>
2021-06-12 11:22:15 +0200

    include JWT revocation on user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-    belongs_to :team
-    has_many :users_foods
-    has_many :users_hobbies
-    has_many :foods, through: :users_foods
-    has_many :hobbies, through: :users_hobbies
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    # validates :password, presence: true   # We might remove this, if we use has_secure_password for bcrypt
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  belongs_to :team
+  has_many :users_foods
+  has_many :users_hobbies
+  has_many :foods, through: :users_foods
+  has_many :hobbies, through: :users_hobbies
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true   # We might remove this, if we use has_secure_password for bcrypt
 end
