c5f5eebf27b574c544349c44d7fe625c172b80c7
Rick <dirklo@gmail.com>
2021-06-12 13:03:41 +0200

    fix login routes and typos in seeds

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
-}
+  } 
   resources :companies
   # resources :users
   resources :teams
