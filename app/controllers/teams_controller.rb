9f3cf83703b3277d56135eea0f3ba4704a25ca86
denalibalser <denali.balser167@gmail.com>
2021-06-12 18:23:28 -0500

    remove condition statements from action controllers, add before_action to authenticate users in teams and companies controllers

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,24 +1,17 @@
 class TeamsController < ApplicationController
+    before_action :authenticate_user!
 
     def create  
         #admin_user should be only one able to create team
     end 
 
     def show 
-        if current_user
-            @current_users_team = Team.find_by_id(current_user.team_id)
-            render json: { status:201, team: @current_users_team}
-        else 
-            render json: { status: 500, error: "*No Team to display currently."}
-        end
+        @current_users_team = Team.find_by_id(current_user.team_id)
+        render json: { status:201, team: @current_users_team}
     end
 
     def index 
-        if current_user
-            @teams = Team.all  
-            render json: {status:201, teams: @teams}
-        else 
-            render json: { status: 500, error: "*You need to be logged in to access this data."}
-        end 
+        @teams = Team.all  
+        render json: {status:201, teams: @teams}
     end 
 end
