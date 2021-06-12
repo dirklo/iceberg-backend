e4641fb1db7be2423db2267c906ca0845dfcb13b
denalibalser <denali.balser167@gmail.com>
2021-06-12 12:17:52 -0500

    remove duplicate sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,5 @@ Rails.application.routes.draw do
   resources :hobbies
   resources :foods
 
-  resources :sessions, only: [:create]  
-  # get '/logged_in', to: 'sessions#logged_in'
-  delete 'logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
