cbfbdcc018752dcacbd880c695896bee884b9306
github username <github email address>
2021-06-12 12:13:26 -0400

    renders users index data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def users_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password, food_ids: [], hobby_ids: [], foods_attributes: [:name])
+    end
 end
