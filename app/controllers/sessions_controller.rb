e30cde20acf91a8a1ef414e3c2bca2df8bc00f64
denalibalser <denali.balser167@gmail.com>
2021-06-12 11:35:56 -0500

    add routes and controller actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
-    
+    def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:id] = @user.id
+            render json: { status: 201, user: @user, logged_in: true } 
+        else 
+            render json: { status: 401, error: "*User not found or incorrect password", logged_in: false}
+        end
+    end 
+
+    # def logged_in
+    #     if logged_in?
+    #       render json: { status: 201, user: current_user, logged_in: true}
+    #     else
+    #       render json: { status: 400, user: {}, logged_in: false}
+    #     end
+    # end
+
+    def logout
+        reset_session 
+        render json: { status: 200, user: {}, logged_in: false } 
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-       
-=======
 
     def index
         users = User.all
@@ -11,5 +8,4 @@ class UsersController < ApplicationController
     def users_params
         params.require(:user).permit(:first_name, :last_name, :username, :email, :password, food_ids: [], hobby_ids: [], foods_attributes: [:name])
     end
->>>>>>> b5c90fab3f33e054d44bd219c434ccceb72c76d9
 end
