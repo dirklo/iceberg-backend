db7913f57e33fa5a338a7031c2bd3c4bc5d90b72
denalibalser <denali.balser167@gmail.com>
2021-06-12 11:43:58 -0500

    remove sessions controller and methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::API
-    def logged_in? 
-        !!current_user
-    end
 
-    def current_user 
-        @current_user ||= User.find(session[:id]) if session[:id]
-    end 
 end
