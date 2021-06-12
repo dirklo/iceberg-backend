951c9781f97cff64542bbc510b288dcc3b298648
Rick <dirklo@gmail.com>
2021-06-12 10:39:01 +0200

    add sessions and registrations controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :companies
-  resources :users
+  # resources :users
   resources :teams
   resources :hobbies
   resources :foods
