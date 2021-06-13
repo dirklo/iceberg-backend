ed25813d84b787dee2e8d8b9777c65aa581bb51c
denalibalser <denali.balser167@gmail.com>
2021-06-13 08:18:46 -0500

    add comments to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,17 +1,14 @@
 class TeamsController < ApplicationController
-    
-
-    def create  
+    # def create  
         #admin_user should be only one able to create team
-    end 
+    # end 
 
     def show 
-        @current_users_team = Team.find_by_id(current_user.team_id)
+        @current_users_team = Team.find_by_id(current_user.team_id) #current_user Devise helper method inaccessible here
         render json: { status:201, team: @current_users_team}
     end
 
     def index 
-        binding.pry
         @teams = Team.all  
         render json: {status:201, teams: @teams}
     end 
