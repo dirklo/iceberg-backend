241ff10714d7843807805d2fa7f80e8ea1719718
rohanphillips <rohan@solutions-insurance.com>
2021-07-02 14:51:04 -0500

    users show testing setup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,4 +17,27 @@ RSpec.describe Users, type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /users/:user_id' do
+    before { get "/users/#{user_id}"}
+    context 'when request is valid' do
+      it 'returns user' do
+        expect(json["id"]).to eq(user_id)
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+  describe 'GET /users/:user_id' do
+    before { get "/users/#{-1}"}
+    context 'when request is invalid' do
+      it 'returns failure message' do
+        expect(json).to include("message" => "User is not found")
+      end
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
 end
