ad336561217c4cb66d647dd7a271ac47d7c18cdb
rohanphillips <rohan@solutions-insurance.com>
2021-07-02 14:30:06 -0500

    build out users requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Users, type: :request do
   # initialize test data
-  let!(:users) {create_list(:user,2)}
-  let!(:user_id) {categories.first.id}
+  let!(:users) { create_list(:user,2) }
+  let!(:user_id) { users.first.id }
 
   # Test suite for GET /user
   describe "GET /users" do
     # make HTTP get request before each example
-    before {get '/api/v1/users'}
+    before {get '/users'}
     it 'returns users' do
       expect(json).not_to be_empty
       expect(json.size).to (eq 2)
